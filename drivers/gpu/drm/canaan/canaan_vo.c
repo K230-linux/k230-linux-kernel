d28d96a54684ddc81d701a4e107eb07922d5b4f1
Sungjoon Moon <sumoon@seoulsaram.org>
2025-12-29 04:06:21 +0900

    drm/canaan: Add missing linux/delay.h include for msleep()
    Fixes build error:
      error: implicit declaration of function 'msleep'

--- drivers/gpu/drm/canaan/canaan_vo.c
+++ drivers/gpu/drm/canaan/canaan_vo.c
@@ -14,6 +14,7 @@
 #include <linux/device.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
+#include <linux/delay.h>
 
 #include <drm/drm_atomic.h>
 #include <drm/drm_atomic_helper.h>
